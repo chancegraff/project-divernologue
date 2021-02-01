fdf56dc6c0035ca81ddd6735859213616a2e107e
chancegraff <cgraffcode@gmail.com>
2021-02-01 12:20:53 -0600

    Refactor into template

---
+++ components/Template.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import styles from "./Template.module.scss";
+
+export default function Splash({
+  children,
+}) {
+  return (
+    <div>
+      <Head>
+        <title>Divernologue</title>
+        <link rel="icon" href="/favicon.svg" />
+        <link rel="mask-icon" href="/mask-icon.svg" color="#ffffff" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <header className={styles.header}>
+        <a><img src="/logo-long.svg" /></a>
+        <a><img src="/social-instagram.svg" /></a>
+      </header>
+      <main className={styles.main}>
+        {children}
+      </main>
+      <footer className={styles.footer}>
+        <small className={styles.small}>© 2021 Divernologue LLC</small>
+        <img src="/logo-small.svg" />
+      </footer>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Template from "../components/Template";
 import Hero from "../sections/Hero";
 import Info1 from "../sections/Info1";
 import Info2 from "../sections/Info2";
@@ -7,31 +7,12 @@ import Form from "../sections/Form";
 
 export default function Splash() {
   return (
-    <div>
-      <Head>
-        <title>Divernologue</title>
-        <link rel="icon" href="/favicon.svg" />
-        <link rel="mask-icon" href="/mask-icon.svg" color="#ffffff" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-      <header>
-        <div>
-          <img src="/logo-long.svg" />
-          <img src="/social-instagram.svg" />
-        </div>
-      </header>
-      <main>
-        <Hero />
-        <Info1 />
-        <Info2 />
-        <Info3 />
-        <Form />
-      </main>
-      <footer>
-        <small className="small-on-dark">© 2021 Divernologue LLC</small>
-        <img src="/logo-small.svg" />
-      </footer>
-    </div>
+    <Template>
+      <Hero />
+      <Info1 />
+      <Info2 />
+      <Info3 />
+      <Form />
+    </Template>
   )
 }
